89665b7f56b28eef901207c112c86f639897d725
CrystalViking <eugene.polyakow@gmail.com>
2020-12-04 21:29:58 +0100

    fix MessageClient.c

--- MessageClient.c
+++ MessageClient.c
@@ -1,60 +1,51 @@
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
+#include <stdio.h>
 #include <netinet/in.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-
-int main()
+int main(void)
 {
 	char abcd[512];
-	char message[256];
-
+	char message[1024];
 	struct sockaddr_in client_address;
 	unsigned int port;
 	int client_socket;
-	int n = 0;
 
-	printf("Please, provide the server IP: ");
+	printf("Please provide the server IP: ");
 	scanf("%s", abcd);
-	printf("Please, provide the port address: ");
+	printf("Please provide the port number: ");
 	scanf("%u", &port);
-	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	client_socket = socket(PF_INET, SOCK_STREAM, 0);
 	client_address.sin_family = AF_INET;
 	client_address.sin_port = htons(port);
 	client_address.sin_addr.s_addr = inet_addr(abcd);
-
-	int connect_status = (client_socket, (struct sockaddr*) &client_address, sizeof(client_address));
-
-	if(connect_status < 0)
+	if (connect(client_socket, (struct sockaddr *)&client_address,
+				sizeof(client_address)) < 0)
 	{
-		printf("Connect failed\n");
+		printf("Connection failed.\n");
 		return 1;
 	}
-	else
-	{
-		printf("Connected to the server...\n");
-	}
+	printf("Connected\n");
+	fgetc(stdin);
 
-	while(1)
+	while (1)
 	{
-		bzero(message, sizeof(message));
-		printf("Enter the message: ");
-		n = 0;
-		fflush(stdout); fgetc(stdin);
-		//while((message[n++] = getchar()) != '\n');
-		send(client_socket, message, strlen(message),0);
-
-		if ((strncmp(message, "exit", 4)) == 0) {
+		printf("Enter message: ");
+		fflush(stdout);
+		fgets(message, 1024, stdin);
+		if (strncmp("exit", message, 4) == 0)
+		{
+			send(client_socket, message, strlen(message), 0);
 			printf("Client exit...\n");
 			break;
 		}
+		send(client_socket, message, strlen(message), 0);
+		printf("message sent\n");
 	}
 
 	close(client_socket);
 	return 0;
 }
-
