824084a33190b7c0048d7f880d993f4d5a9b14f9
CrystalViking <eugene.polyakow@gmail.com>
2020-12-04 21:30:42 +0100

    fix MessageServer.c

--- MessageServer.c
+++ MessageServer.c
@@ -1,79 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <string.h>
-
-#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
+#include <stdio.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int main()
+int main(void)
 {
-	char message[256];
+  unsigned int port;
+  char message[1024];
+  int server_socket, client_socket;
+  struct sockaddr_in server_address, sender;
+  socklen_t length = sizeof(struct sockaddr_in);
 
+  printf("Which port should I listen to : ");
+  scanf("%u", &port);
+  server_socket = socket(PF_INET, SOCK_STREAM, 0);
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(port);
+  server_address.sin_addr.s_addr = INADDR_ANY;
+  if (bind(server_socket, (struct sockaddr *)&server_address,
+           sizeof(server_address)) < 0)
+  {
+    printf("Bind failed.\n");
+    return 1;
+  }
+  if (listen(server_socket, 10) < 0)
+  {
+    printf("Listen failed.\n");
+    return 1;
+  }
+  printf("Waiting for connection...\n");
 
-	// prompt user to input the port number
-	unsigned int port;
-	printf("Which port should I listen to? : ");
-	scanf("%u", &port);
+  if ((client_socket = accept(server_socket,
+                         (struct sockaddr *)&sender,
+                         &length)) < 0)
+  {
+    printf("Connection error\n");
+  }
+  int exit_status = 0;
+  while (exit_status == 0)
+  {
 
-	// create the server socket
-	int server_socket;
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	// define the server address
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(port);
-	server_address.sin_addr.s_addr = INADDR_ANY;
-
-	//bind the socket
-	int bind_status = bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-	//bind check
-	if(bind_status < 0)
-	{
-		printf("Bind failed");
-		return 1;
-	}
-
-	// start to listen
-	int listen_status = listen(server_socket, 10);
-
-	if(listen_status < 0)
-	{
-		printf("Listen failed");
-		return 1;
-	}
-
-	printf("Waiting for connection...\n");
-
-	//accepting connection
-	struct sockaddr_in client_address;
-	socklen_t client_address_length = sizeof(struct sockaddr_in);
-	int client_socket = accept(server_socket, (struct sockaddr*) &client_address, &client_address_length);
-
-
-
-	while(1)
-	{
-		bzero(message, 256);
-
-		//read the message from client and copy to buffer
-		recv(server_socket, message, sizeof(message), 0);
-		printf("From client: %s", message);
-		if (strncmp("exit", message, 4) == 0)
-		{
-			printf("Server exit...\n");
-			break;
-		}
-	}
-
-
-	close(server_socket);
-
-	return 0;
+    memset(message, 0, 1024);
+    recv(client_socket, message, 1024, 0);
+    if (strncmp("exit", message, 4) == 0)
+    {
+      printf("Server exit...\n");
+      exit_status = 1;
+    }
+    else
+    {
+      printf("Message from %s: %s\n",
+             inet_ntoa(sender.sin_addr),
+             message);
+    }
+  }
+  close(client_socket);
+  close(server_socket);
+  return 0;
 }
